3263feb2520c8606cedadd2e4291ab2d9611650b
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-02-23 23:30:18 +0100

    Tried stuff and succeeded

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,62 +6,65 @@ let getUsername = document.querySelector(".username-input");
 const winMessage = "You win! Yay!";
 const tieMessage = "It's a tie! Everybody wins!";
 const loseMessage = "Yaiks, that plan backfired! Better luck next time!";
-const rockVsScissors = "Rock crushes Scissors"
-const rockVsLizward = "Rock crushes Lizard"
-const paperVsRock = "Paper covers Rock"
-const paperVsSpock = "Paper disproves Spock"
-const scissorsVsPaper = "Scissors cut Paper"
-const scissorsVsLizard = "Scissors decapitate Lizard"
-const lizardVsSpock = "Lizard poisons Spock"
-const lizardVsPaper = "Lizard eats Paper"
-const spockVsScissors = "Spock smashes Scissors"
-const spockVsRock = "Spock vaporizes Rock"
+const rockVsScissors = "Rock crushes Scissors";
+const rockVsLizward = "Rock crushes Lizard";
+const paperVsRock = "Paper covers Rock";
+const paperVsSpock = "Paper disproves Spock";
+const scissorsVsPaper = "Scissors cut Paper";
+const scissorsVsLizard = "Scissors decapitate Lizard";
+const lizardVsSpock = "Lizard poisons Spock";
+const lizardVsPaper = "Lizard eats Paper";
+const spockVsScissors = "Spock smashes Scissors";
+const spockVsRock = "Spock vaporizes Rock";
 
+//done
 // Scoreboard variables
 let userScore = 0;
-let getUserScore = document.getElementById("user-score");
+let userScoreElement = document.getElementById("user-score");
 let computerScore = 0;
-let getComputerScore = document.getElementById("computer-score");
+let computerScoreElement = document.getElementById("computer-score");
 
+//done
 // Choice-buttons variables
 const choicesArray = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
-// following variables are for add.Event.Listener("click")
-const rockVar = document.getElementById("rock");
-const paperVar = document.getElementById("paper");
-const scissorsVar = document.getElementById("scissors");
-const lizardVar = document.getElementById("lizard");
-const spockVar = document.getElementById("spock");
-const choiceButtons = (rockVar, paperVar, scissorsVar, lizardVar, spockVar);
+
 // Outcome variables
 let getOutcome = document.getElementById("outcome");
 
-let displayUserChoiceMessage = document.querySelector(".display-user-choice-message>p");
-let displayUserChoice = document.querySelector(".display-user-choice>p");
-
-let displayComputerChoiceMessage = document.querySelector(".display-computer-choice-message>p");
-let displayComputerChoice = document.querySelector(".display-computer-choice>p");
-
-// Event listeners for choice-buttons
-rockVar.addEventListener("click", function () {
-  compareChoices(choicesArray[0]);
+//done
+// Variables for user feedback to game status for improved UX.
+// Apply method (document.getElementByID) to retrieve references for elements from the DOM
+//  for functions to display the updated choices and results in the DOM.
+// Using let because  choices and results are dynamic.
+let userChoiceElement = document.getElementById("user-choice");
+let computerChoiceElement = document.getElementById("computer-choice");
+let resultElement = document.getElementById("result");
+// done
+// Event listeners for choice-buttons - a method to retrieve references for button elements from the DOM (document.getElementByID) and
+//  add button references to event listener method (addEventListeners).
+//When buttons are clicked, the function compareChoices, the game logic, is called.
+//The string for each choice button from the array choices is passed as an argument to the function compareChoices.
+document.getElementById("rock").addEventListener("click", function () {
+  compareChoices("Rock");
+});
+// done
+document.getElementById("paper").addEventListener("click", function () {
+  compareChoices("Paper");
+});
+// done
+document.getElementById("scissors").addEventListener("click", function () {
+  compareChoices("Scissors");
+});
+// done
+document.getElementById("lizard").addEventListener("click", function () {
+  compareChoices("Lizard");
+});
+// done
+document.getElementById("spock").addEventListener("click", function () {
+  compareChoices("Spock");
 });
 
-paperVar.addEventListener("click", function () {
-  compareChoices(choicesArray[1]);
-});
-
-scissorsVar.addEventListener("click", function () {
-  compareChoices(choicesArray[2]);
-});
-
-lizardVar.addEventListener("click", function () {
-  compareChoices(choicesArray[3]);
-});
-
-spockVar.addEventListener("click", function () {
-  compareChoices(choicesArray[4]);
-});
-
+//to-do
 // Store username
 function storeUsername() {
   localStorage.setItem(user, value);
@@ -70,31 +73,58 @@ function storeUsername() {
   console.log(getUsername);
 }
 
+// done
 function generateComputerChoice() {
   return choicesArray[Math.floor(Math.random() * choicesArray.length)];
 }
 
-// Increment Scores
-function incrementUserScore() {
-  userScore++;
-  getUserScore.innerHTML = userScore;
+//done
+// Function to update the displayed user and computer choices in the DOM
+//  to increase UX
+function updateChoiceElements(
+  userChoiceElement,
+  computerChoiceElement,
+  userChoice,
+  computerChoice
+) {
+  userChoiceElement.innerHTML = "Your choice: " + userChoice;
+  computerChoiceElement.innerHTML = "Computer choice: " + computerChoice;
 }
 
+// done
+// Functions to increment scores
+function incrementUserScore() {
+  userScore++;
+  updateScoreElement(userScoreElement, userScore);
+}
+
+//done
 function incrementComputerScore() {
   computerScore++;
-  getComputerScore.innerHTML = computerScore;
+  updateScoreElement(computerScoreElement, computerScore);
+}
+
+//done
+// Function to display updated scores in the DOM
+function updateScoreElement(element, score) {
+  element.innerHTML = score;
+}
+
+function updateResultElement(resultElement, result) {
+  resultElement.innerHTML = result;
 }
 
 //function showOutcome (displayUserChoice) {
-  //if (userChoice === "Rock" && computerChoice === "Paper") return
-    //displayUserChoice.innerHTML = rockVsScissors;
-  //}
+//if (userChoice === "Rock" && computerChoice === "Paper") return
+//displayUserChoice.innerHTML = rockVsScissors;
+//}
 
 // Remember to include bug . see README, Bugs: Forgot to close parenthesis (see below as ### heading)
 
 // Function for when the outcome of comparing the choices is that user wins to increment user's score
 function userWins(userChoice, computerChoice) {
   incrementUserScore();
+  updateResultElement(result, winMessage);
   getOutcome.innerHTML =
     "Your choice: " +
     userChoice +
@@ -103,14 +133,10 @@ function userWins(userChoice, computerChoice) {
     computerChoice +
     ": " +
     winMessage;
-    displayUserChoiceMessage.innerHTML = "Your choice: " + userChoice;
-    displayUserChoice.innerHTML = userChoice;
-    displayComputerChoiceMessage.innerHTML = "Computer's choice:";
-    displayComputerChoice.innerHTML = computerChoice;
-    //showOutcome();
 }
 
 function userTies(userChoice, computerChoice) {
+  updateResultElement(result, tieMessage);
   getOutcome.innerHTML =
     "Your choice: " +
     userChoice +
@@ -123,6 +149,7 @@ function userTies(userChoice, computerChoice) {
 
 function userLoses(userChoice, computerChoice) {
   incrementComputerScore();
+  updateResultElement(result, loseMessage);
   getOutcome.innerHTML =
     "Your choice: " +
     userChoice +
@@ -133,12 +160,17 @@ function userLoses(userChoice, computerChoice) {
     loseMessage;
 }
 
+//done
 // Compare choices: deciding which choice wins
 function compareChoices(userChoice) {
   // Randomly generated computer's choice
   let computerChoice = generateComputerChoice();
-  //console.log("User's choice: " + userChoice);
-  //console.log("Computer's choice: " + computerChoice);
+  updateChoiceElements(
+    userChoiceElement,
+    computerChoiceElement,
+    userChoice,
+    computerChoice
+  );
   if (userChoice === computerChoice) {
     userTies(userChoice, computerChoice);
   } else if (
@@ -154,7 +186,6 @@ function compareChoices(userChoice) {
       (computerChoice === "Scissors" || computerChoice === "Rock"))
   ) {
     userWins(userChoice, computerChoice);
-    //showOutcome();
   } else {
     userLoses(userChoice, computerChoice);
   }
